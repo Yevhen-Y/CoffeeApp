f2b1c1a2fea45d5d81e115c4ed42ee59f1af4ab2
evan <text2evan@gmail.com>
2016-07-13 20:01:24 +0300

    sort files

--- coffeeShopsCtrl.js
+++
@@ -1,7 +0,0 @@
-myApp.controller('coffeeShopsCtrl', [ '$scope','$http', function($scope, $http) {
-//Getting data from json file
-  $http.get('coffeeShops.json').then(function(response) {
-  $scope.coffeeShops = response.data.methods;
-})
-
-}]);
--- methodsCtrl.js
+++
@@ -1,8 +0,0 @@
-// Controller for METHODS PAGE
-myApp.controller('methodsCtrl', [ '$scope','$http', function($scope, $http) {
-//Getting data from json file
-  $http.get('JSON_files/coffeeMethods.json').then(function(response) {
-  $scope.methods = response.data.methods;
-})
-
-}]);
